d910db90fd496416ab702da4b41564fd9e9a8b13
RickyAnchores <ricky23anchores@gmail.com>
2021-03-15 23:47:57 +0000

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+let logo = document.querySelector('#logo');
+let footer = document.querySelector('#footer');
+
+logo.addEventListener('click', () => {
+	document.body.style.background = "black";
+  document.body.style.color = "white";
+  document.body.style.transition = "0.5s ease-in";
+  
+  footer.style.background = "grey";
+  footer.style.color = "#333";
+})
